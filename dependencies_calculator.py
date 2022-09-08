cd1cc403cb3bdfe9acc21ed8f2355897b79d5dac
JuanDa14Sa <coziestwharf@gmail.com>
2022-09-08 16:40:28 -0500

    Minimal Cover in standard form

--- dependencies_calculator.py
+++ dependencies_calculator.py
@@ -8,7 +8,7 @@ def print_arrow_form(dependencias):
 def prepro(dependencias):
     depend_list = []
     for depend in dependencias:
-        left, right = depend.split(':')
+        left, right = depend.split('->')
         left_ = left.split(',')
         right_ = right.split(',')
         depend_list.append([left_,right_])
@@ -38,37 +38,88 @@ def closure_attr(attribute,dependencias, preprocess= True):
             break
     return result
 
+def right_side_decomposition(dependencias):
+    new_dependencias = dependencias.copy()
+    for depend in new_dependencias:
+        left, right = depend[0],depend[1]
+        # found = False
+        if len(right)!=1:
+            for b in right:
+                # print(b)
+                new_dependencias.append([left,[b]]) 
+                # found = True
+                if b==right[-1]:
+                    new_dependencias.remove(depend)
+                    # found = False
+
+    return new_dependencias
+
+
+
+
 def canonical_cover(depedencias):
     cover = prepro(depedencias)
+    cover = right_side_decomposition(cover)
     for depend in cover:   #Eliminamos atributos extraños a izquierda. 
         left, right = depend[0], depend[1]
         for a in left:
             left_tmp = left.copy()
-            if left_tmp.remove(a) is not None and is_subset(right, closure_attr(left_tmp,cover,preprocess=False)):
+            left_tmp.remove(a)
+            # print(left_tmp)
+            # print(left)
+            # print(cover)
+            # print('Closure of',left_tmp,':   ',closure_attr(left_tmp,cover,preprocess=False))
+            if left_tmp is not None and is_subset(right, closure_attr(left_tmp,cover,preprocess=False)):
                 cover.remove([left,right])
                 cover.append([left_tmp,right])
+                break
+    # print('Cover after left reduction: ')
+    # print_arrow_form(cover)
 
-    for depend in cover:
+    for depend in cover: #Eliminamos atributos extraños a derecha
         left, right = depend[0], depend[1]
         for b in right:
             right_tmp = right.copy()
             right_tmp.remove(b)
             cover_tmp = cover.copy()
+            # print('Right_tmp:',right_tmp)
+            # print('Right:',right)
+            # print(b)
+            # print(left,right)
+            # print_arrow_form(cover_tmp)
             cover_tmp.remove([left,right])
-            cover_tmp.append([left,right_tmp])
+            cover_tmp.insert(0,[left,right_tmp])
+            # print('After remove:')
+            # print_arrow_form(cover_tmp)
+            # print('Closure of',left,':   ',closure_attr(left,cover_tmp,preprocess=False))
             if b in closure_attr(left, cover_tmp,preprocess=False):
                 cover = cover_tmp
+    # print(cover)
+
+    # print(len(cover))
+    cover_ = cover.copy()
+    for depend in cover:
+        # print(depend)
+        right = depend[1]
+        # print(right==[])
+        if right==[] or right is None:
+            cover_.pop(0)
+    cover = cover_    
     return cover
 
 
 
 
-# dependencias = ['A:B', 'A:C', 'C,G:H', 'C,G:I', 'B:H']
-dependencias = [ 'B:C', 'A:B', 'A:B,C']
+
+# dependencias = ['A->B,C', 'B->C', 'A->B', 'A,B->C']
+# dependencias = [ 'B->C','A->B','A->B,C']
+dependencias = ['A,C->G', 'D->E,G', 'B,C->D', 'C,G->B,D','A,C,D->B','C,E->A,G']
 # attribute = ['A', 'G']
-attribute = ['B','D']
+# attribute = ['B','D']
 
 
-print(canonical_cover(dependencias))
+# print_arrow_form(right_side_union(prepro(dependencias)))
 
+print_arrow_form(canonical_cover(dependencias))
 # print(closure_attr(attribute,dependencias))
+# print(tmp[0][1]==[])
